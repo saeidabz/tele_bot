8d789fd9666dc7c2be45557a716a7e6c54e0088c
saeidabz <saeidabbaszadeh.91@gmail.com>
2024-02-22 20:36:49 +0330

    Add Logging

--- bot.py
+++ bot.py
@@ -1,19 +1,21 @@
 import os
-
 import telebot
+import logging
+import sys
+
+#  lOG
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 from dotenv import load_dotenv
+
 
 load_dotenv()
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 bot = telebot.TeleBot(BOT_TOKEN)
-
 @bot.message_handler(commands=['start', 'hello'])
 def send_welcome(message):
     bot.reply_to(message, "Howdy, how are you doing?")
-
 @bot.message_handler(func=lambda msg: True)
 def echo_all(message):
     bot.reply_to(message, message.text)
-
-
+print("Salam")
 bot.infinity_polling()
